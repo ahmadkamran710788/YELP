9231bc3db443052bcb0e07647b47864cffe61df0
ahmadkamran710788 <--list>
2024-03-21 02:03:04 +0500

    we are know ok

--- server/db/index.js
+++ server/db/index.js
@@ -3,11 +3,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "yelp",
-  password: "710788",
-  port: 5432,
+  user: process.env.PGHOST,
+  host: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 module.exports = {
